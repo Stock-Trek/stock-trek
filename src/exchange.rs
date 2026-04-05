26a79323fc710a49c2db5bf6cbf953bb488281c3
Nathan <an.email.com>
2026-04-05 15:14:58 +0100

    polish

--- src/exchange.rs
+++ src/exchange.rs
@@ -1,13 +1,5 @@
 use crate::{market_data::market::Market, prelude::TradingPair};
 use std::collections::HashMap;
-use strum::EnumIter;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter)]
-#[non_exhaustive]
-#[repr(u8)]
-pub enum ExchangeName {
-    Binance,
-}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
