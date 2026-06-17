0371e128bd2264839ce248e50c714330003e095f
Nathan <an.email.com>
2026-06-17 13:29:21 +0100

    remove order id

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Add to your Cargo.toml:
 
 ```rs
 [dependencies]
-stock-trek = "0.8.13"
+stock-trek = "0.8.14"
 ```
 
 ## Python Bindings (coming soon)
--- src/cex/order_response.rs
+++ src/cex/order_response.rs
@@ -1,8 +1,7 @@
-use crate::cex::{order_id::OrderId, order_tag::OrderTag};
+use crate::cex::order_tag::OrderTag;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct OrderResponse {
-    pub id: OrderId,
     pub tag: OrderTag,
 }
